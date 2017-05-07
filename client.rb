522a3577dd1da42a26ed0010775b774050e2d773
sorefull <olegh.cherednichenko@gmail.com>
2017-05-07 19:37:59 +0300

    tcp

--- client.rb
+++ client.rb
@@ -1,11 +1,41 @@
 require 'packetgen'
 require 'pry'
 require "awesome_print"
+require 'optparse'
+
+# INTERFACE = 'lo'
+# PROTOCOL = 'ip'
+# DST_IP = '127.0.0.1'
+# SRC_IP = '127.0.0.1'
+
+OptionParser.new do |parser|
+  parser.banner = "Usage: rvmsudo ruby client.rb -i INTERFACE -p PROTOCOL -s SRC_IP -d DST_IP"
+
+  parser.on('-i interface') do |interface|
+    INTERFACE = interface
+  end
+
+  parser.on('-p protocol') do |protocol|
+    PROTOCOL = protocol
+  end
+
+  parser.on('-s src') do |src|
+    SRC_IP = src
+  end
+
+  parser.on('-d dst') do |dst|
+    DST_IP = dst
+  end
+end.parse!
+
+raise OptionParser::MissingArgument if INTERFACE.nil? || PROTOCOL.nil?  || SRC_IP.nil?  || DST_IP.nil?
 
 # http://biot.com/capstats/bpf.html BPF syntax filter
+filter = "#{PROTOCOL} dst #{DST_IP} and ip src #{SRC_IP}" if PROTOCOL == 'ip'
+filter = "#{PROTOCOL} and dst port 80" if PROTOCOL == 'tcp'
+
 loop do
-  PacketGen.capture(iface: 'lo', filter: 'ip dst 127.0.0.1 and ip src 127.0.0.1') do |packet|
-    # do_stuffs_with_packet
+  PacketGen.capture(iface: INTERFACE, filter: filter) do |packet|
     ap packet
   end
 end
--- host.rb
+++ host.rb
@@ -3,7 +3,7 @@ require 'pry'
 require "awesome_print"
 
 1.times do
-  packet = PacketGen.gen('IP', src: '127.0.0.1', dst: '127.0.0.1')
+  packet = PacketGen.gen('TCP', src: '127.0.0.1', dst: '127.0.0.1', port: 80)
   packet.to_w('lo')
 
   ap packet
